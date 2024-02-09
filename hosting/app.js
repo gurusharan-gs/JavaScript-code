453972ac73189e273e2dd5640ac54f6570ac182a
Gurusharan <gurusharanulagadde143@gmail.com>
2024-02-09 13:05:40 +0530

    js

---
+++ hosting/app.js
@@ -0,0 +1,20 @@
+//  Hosting in JS
+
+console.log(x);
+getName();
+
+var x = 5;
+
+function getName() {
+  console.log("Hello World.!");
+}
+
+/*-----------------------------------*/
+
+let n = 20;
+function myName() {
+  console.log("guru");
+}
+
+myName();
+console.log(n);
