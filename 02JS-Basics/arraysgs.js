2fe35e483882e97a9629c3c0dd714d053929f0b6
Gurusharan <gurusharanulagadde143@gmail.com>
2024-02-21 14:54:47 +0530

    arrays

---
+++ 02JS-Basics/arraysgs.js
@@ -0,0 +1,40 @@
+/**
+ * array :--
+ * array is used to store collection of values or multiple items
+ * like list of names or list of numbers so no
+ * arrays store different types of data types
+ * arrays are zero-indexed which means in array count start with first index 0 and second index 1 so on
+ * arrays have so many methods
+ * array is mutable means values are changes in array we update values in array
+ *
+ */
+
+let array = new Array(1, 2, 3, 4, 5, 6, 7);
+// console.log(array);
+
+let arr = [1, 2, 3, 4, 5, 6];
+// console.log(arr);
+// console.log(arr.concat(array));
+// console.log(arr.push(9));
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr.unshift(9));
+// console.log(arr.shift());
+
+// console.log(arr)
+// console.log(arr.reverse())
+
+// slice
+// console.log(arr); // [ 1, 2, 3, 4, 5, 6 ]
+// console.log(arr.slice(0, 3)); //  1, 2, 3 ]
+// console.log(arr); // [ 1, 2, 3, 4, 5, 6 ]
+
+// splice
+// console.log(arr); // [ 1, 2, 3, 4, 5, 6 ]
+// console.log(arr.splice(0, 3)); // [ 1, 2, 3 ]
+// console.log(arr); // [ 4, 5, 6 ]
+
+// sort
+// let sorta = [2, 5, 1, 3, 6, 4];
+// console.log(sorta); // [ 2, 5, 1, 3, 6, 4 ]
+// console.log(sorta.sort()); // [ 1, 2, 3, 4, 5, 6 ]
